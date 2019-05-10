2c93ed4fc66ac6cdd06c78d91ef75a6e792ae4e7
luanyoushu <luanyoushu@xiaoheiban.cn>
2019-05-10 15:53:28 +0800

    body_form

--- tenRiceSK/src/main/routeapi.go
+++ tenRiceSK/src/main/routeapi.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"reflect"
 	"strings"
@@ -28,6 +31,48 @@ func usersHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func postHander(w http.ResponseWriter, r *http.Request) {
+	// 表单中的数据
+	pathInfo := strings.Trim(r.URL.Path, "/")
+	r.ParseForm()
+	for k, v := range r.PostForm {
+		fmt.Printf("k:%v\n", k)
+		fmt.Printf("v:%v\n", v)
+	}
+
+	// 解析 body 中的数据
+	bodyss, _ := ioutil.ReadAll(r.Body)
+	fmt.Print(bodyss)
+	var receiver map[string]interface{}
+	json.Unmarshal(bodyss, &receiver)
+	log.Println(receiver["token"])
+
+	parts := strings.Split(pathInfo, "/")
+	w.Header()
+	fmt.Print(parts)
+	var action = ""
+	if len(parts) > 1 {
+		action = strings.Title(parts[1]) + "Action"
+	}
+
+	r.ParseForm()
+	//_, found1 := r.Form["token"]
+	//if !(found1) {
+	//	fmt.Println("非法访问aaal")
+	//	return
+	//}
+
+	ajax := &usersController{}
+	controller := reflect.ValueOf(ajax)
+	method := controller.MethodByName(action)
+	if !method.IsValid() {
+		method = controller.MethodByName("UsersAction")
+	}
+	requestValue := reflect.ValueOf(r)
+	responseValue := reflect.ValueOf(w)
+	method.Call([]reflect.Value{responseValue, requestValue})
+}
+
+func putHander(w http.ResponseWriter, r *http.Request) {
 	pathInfo := strings.Trim(r.URL.Path, "/")
 	r.ParseForm()
 	for k, v := range r.PostForm {
@@ -44,11 +89,11 @@ func postHander(w http.ResponseWriter, r *http.Request) {
 	}
 
 	r.ParseForm()
-	_, found1 := r.Form["token"]
-	if !(found1) {
-		fmt.Println("非法访问aaal")
-		return
-	}
+	//_, found1 := r.Form["token"]
+	//if !(found1) {
+	//	fmt.Println("非法访问aaal")
+	//	return
+	//}
 
 	ajax := &usersController{}
 	controller := reflect.ValueOf(ajax)
@@ -59,55 +104,39 @@ func postHander(w http.ResponseWriter, r *http.Request) {
 	requestValue := reflect.ValueOf(r)
 	responseValue := reflect.ValueOf(w)
 	method.Call([]reflect.Value{responseValue, requestValue})
-}
-
-func putHander(w http.ResponseWriter, r *http.Request) {
-	pathInfo := strings.Trim(r.URL.Path, "/")
-	parts := strings.Split(pathInfo, "/")
-
-	var action = ""
-	if len(parts) > 1 {
-		action = strings.Title(parts[1]) + "Action"
 	}
-
-	r.ParseForm()
-	_, found1 := r.Form["username"]
-	if !(found1) {
-		fmt.Println("非法访问")
-		return
-	}
-
-	requestValue := reflect.ValueOf(r)
-	responseValue := reflect.ValueOf(w)
-
-	ajax := &usersController{}
-	controller := reflect.ValueOf(ajax)
-	method := controller.MethodByName(action)
-	method.Call([]reflect.Value{responseValue, requestValue})
-}
 
 
 func deleteHander(w http.ResponseWriter, r *http.Request) {
 	pathInfo := strings.Trim(r.URL.Path, "/")
-	parts := strings.Split(pathInfo, "/")
+	r.ParseForm()
+	for k, v := range r.PostForm {
+		fmt.Printf("k:%v\n", k)
+		fmt.Printf("v:%v\n", v)
+	}
 
+	parts := strings.Split(pathInfo, "/")
+	w.Header()
+	fmt.Print(parts)
 	var action = ""
 	if len(parts) > 1 {
 		action = strings.Title(parts[1]) + "Action"
 	}
 
 	r.ParseForm()
-	_, found1 := r.Form["username"]
-	if !(found1) {
-		fmt.Println("非法访问")
-		return
-	}
-
-	requestValue := reflect.ValueOf(r)
-	responseValue := reflect.ValueOf(w)
+	//_, found1 := r.Form["token"]
+	//if !(found1) {s
+	//	fmt.Println("非法访问aaal")
+	//	return
+	//}
 
 	ajax := &usersController{}
 	controller := reflect.ValueOf(ajax)
 	method := controller.MethodByName(action)
+	if !method.IsValid() {
+		method = controller.MethodByName("UsersAction")
+	}
+	requestValue := reflect.ValueOf(r)
+	responseValue := reflect.ValueOf(w)
 	method.Call([]reflect.Value{responseValue, requestValue})
 }
